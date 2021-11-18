91e68b0b291a3568b046fd13d194f1936f5beffd
Davaskye DeBique <Davaskye@gmail.com>
2021-11-17 23:22:18 -0400

    Basic Javascript and Php implementation

--- ProjectFiles/app.js
+++ ProjectFiles/app.js
@@ -1,1 +1,14 @@
 "use strict";
+
+let httpRequest = new XMLHttpRequest();
+
+window.onload = function(){ 
+
+    let logout_button = document.getElementById("logout-btn");
+   
+    logout_button.addEventListener("click", function(event){
+        event.preventDefault()
+        console.log("Hey. This logs you out!");
+    });
+
+}
