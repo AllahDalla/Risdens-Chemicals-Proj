f2b9df18c1ac2af40d7011c7f48d5816e64e6e4a
AllahDalla <alromariodavis@gmail.com>
2021-11-19 03:17:08 -0500

    Javascript and html for manage stock added

--- ProjectFiles/app.js
+++ ProjectFiles/app.js
@@ -1,14 +1,30 @@
 "use strict";
 
-let httpRequest = new XMLHttpRequest();
+// let httpRequest = new XMLHttpRequest();
 
-window.onload = function(){ 
+// window.onload = function(){ 
 
-    let logout_button = document.getElementById("logout-btn");
+//     let logout_button = document.getElementById("logout-btn");
    
-    logout_button.addEventListener("click", function(event){
-        event.preventDefault()
-        console.log("Hey. This logs you out!");
-    });
+//     logout_button.addEventListener("click", function(event){
+//         event.preventDefault()
+//         console.log("Hey. This logs you out!");
+//     });
 
+// }
+
+// Starting the real coding for pages here
+
+
+// This right here runs the entire scipt on FontFaceSetLoadEvent. All functions made will be called inside this function
+window.onload = function(){
+
+}
+
+
+function buttonFucntions(){
+    var sidebarManageStock = document.getElementById("MS-btn")
+    var manageStockAddStockBtn = document.getElementById("add-stock-btn")
+
+    sidebarManageStock.addEventListener()
 }
