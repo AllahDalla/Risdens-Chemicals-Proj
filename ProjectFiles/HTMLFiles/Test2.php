c00c64db83120409e321c4c648d999556b1789d6
AllahDalla <alromariodavis@gmail.com>
2021-11-28 05:39:26 -0500

    Testing Login

---
+++ ProjectFiles/HTMLFiles/Test2.php
@@ -0,0 +1,19 @@
+<?php
+
+include "../Databases/database.php";
+    $role = "admin";
+    $username  = "AllahDalla";
+    $pwd = "password321";
+
+
+    $hash = hash_init("sha1");
+    hash_update($hash, $pwd);
+    $password = hash_final($hash);
+
+    $sql = "SELECT * FROM `users` WHERE role= '$role' AND username= '$username'";
+    $stmt = $conn->query($sql);
+
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo $results[0]['password'];
+
+?>
