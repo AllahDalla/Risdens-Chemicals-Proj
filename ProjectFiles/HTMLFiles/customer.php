430b686fdf096c279b2c0348e81c497e05bbc901
RamonaStanberry <monacstanberry@gmail.com>
2021-11-30 22:42:07 -0500

    Styled customer page

--- ProjectFiles/HTMLFiles/customer.php
+++ ProjectFiles/HTMLFiles/customer.php
@@ -7,12 +7,12 @@
     <link rel="stylesheet" href="../Style.css" />
     <title>Customer Order Page</title>
   </head>
-  <body>
+  <body class="customer-body">
     <div id="main-info">
       <h1>WELCOME TO RISDEN'S CHEMICALS</h1>
       
       
-      <table>
+      <table class="table">
         <caption>List of Items in Stock</caption>
         <thead>
            <tr>
@@ -35,21 +35,21 @@
       <form action="../Databases/database.php?customer-order=cus-order-incoming" method="post">
         <div id="input-fields">
           <label for="ctitle">Title:</label><br>
-          <input type="text" id="ctitle" name="ctitle" required placeholder="Mr/Ms/Mrs"><br> 
+          <input type="cus-text" id="ctitle" name="ctitle" required placeholder="Mr/Ms/Mrs"><br> 
           <label for="cname">Name:</label><br>
-          <input type="text" id="cname" name="cname" required><br> 
+          <input type="cus-text" id="cname" name="cname" required><br> 
           <label for="cp_name">Product Name:</label><br>
-          <input type="text" id="cp_name" name="cp_name" required><br> 
+          <input type="cus-text" id="cp_name" name="cp_name" required><br> 
           <label for="cquantity">Quantity:</label><br>
-          <input type="text" id="cquantity" name="cquantity" required><br> 
+          <input type="cus-text" id="cquantity" name="cquantity" required><br> 
           <label for="cnumber">Telephone Number:</label><br>
-          <input type="text" id="cnumber" name="cnumber" required><br> 
+          <input type="cus-text" id="cnumber" name="cnumber" required><br> 
           <label for="caddress">Delivery Address:</label><br>
-          <input type="text" id="caddress" name="caddress" required><br> 
+          <input type="cus-text" id="caddress" name="caddress" required><br> 
           <label for="ddate">Delivery Date:</label><br>
-          <input type="date" id="ddate" name="ddate" required><br> 
+          <input type="cus-date" id="ddate" name="ddate" required><br> 
         </div>
-          <input type="submit" id="submit-btn" value="Place Order"><br> 
+          <input type="cus-submit" id="cus-submit-btn" value="Place Order"><br> 
       </form>
       </div>
     </div>
